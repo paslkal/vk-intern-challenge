404d629f8e6635ffdded3659ca857aa45a2395df
paslkal <paskalsinda@gmail.com>
2024-10-30 00:13:28 +0300

    create a little bit more tests for Header, also create beforeEach

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,13 +2,24 @@ import {render, screen} from '@testing-library/react'
 import Header from './Header'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-test('render link', () => {
-  render(
-    <Router>
-      <Header/>
-    </Router>
+describe('Render Header', () => {
+  beforeEach(() => 
+    render(
+      <Router>
+        <Header/>
+      </Router>
+    )  
   )
-  const allCatsLink = screen.getByText(/все котики/i)
+
+  test('render link', () => {
+    const allCatsLink = screen.getByText(/все котики/i)
+    
+    expect(allCatsLink).toBeInTheDocument()
+  })
   
-  expect(allCatsLink).toBeInTheDocument()
+  test('render navigation', () => {
+    const navigation = screen.getByRole('navigation')
+
+    expect(navigation).toBeInTheDocument()
+  })
 })
