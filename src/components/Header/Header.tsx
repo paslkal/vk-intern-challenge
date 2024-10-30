974082953db23cb6036994807bd3a85dd99ba38f
paslkal <paskalsinda@gmail.com>
2024-10-30 19:21:48 +0300

    use tailwind in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
-import './Header.css'
 import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
     <header>
-      <nav>
+      <nav className='bg-blue-400 overflow-hidden pl-24 shadow-2xl'>
         <Link 
           to=''
-          className='active'
+          className='
+            bg-blue-500 
+            text-white 
+            hover:bg-blue-700 
+            font-roboto
+            text-base
+            font-normal
+            leading-5
+            tracking-wide-wide
+            text-center
+            float-left
+            no-underline
+            py-8
+            px-5
+          '
         >
           Все котики
         </Link>        
