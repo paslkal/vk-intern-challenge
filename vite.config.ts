0a21ffdd2c034913ba8586690f97215c7d8f1e10
paslkal <paskalsinda@gmail.com>
2024-10-31 19:26:21 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vk-intern-challenge',
   css: {
     postcss: {
       plugins: [tailwindcss()]
