c87eeb52f2875c9cb594868e879e39ec52e15d30
paslkal <paskalsinda@gmail.com>
2025-01-16 16:31:13 +0300

    change readme and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,6 +24,27 @@ export default function Header() {
         >
           Все котики
         </Link>        
+        <Link 
+          to=''
+          className='
+            bg-blue-400 
+            text-slate-300 
+            hover:bg-blue-700 
+            font-roboto
+            text-base
+            font-normal
+            leading-5
+            tracking-wide-wide
+            text-center
+            float-left
+            no-underline
+            py-8
+            px-5
+            
+          '
+        >
+          Любимые котики
+        </Link>        
       </nav>
     </header>
   )
